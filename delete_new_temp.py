6959b49f4fde0538d437ec37d2009dc9ccee8737
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-19 15:46:13 -0400

    Fix matching path formatting and how merging

--- delete_new_temp.py
+++ delete_new_temp.py
@@ -9,11 +9,9 @@ Returns:
 """
 import bagit
 import os
-import pandas
+import pandas as pd
 import re
 import sys
-
-import pandas as pd
 
 
 def find_extra_files(bag):
@@ -22,20 +20,23 @@ def find_extra_files(bag):
     Returns: extras (list) - list of the paths for every file in data but not the manifest
     """
     # List of file paths in the data folder, saved as a dataframe to compare to manifest.
-    # To match the manifest, it needs to start at data.
+    # To match the manifest, it needs to start at data and use forward slashes.
     data_paths = []
     for root, dirs, files in os.walk(os.path.join(bag, 'data')):
         for file in files:
             root_from_data = re.search(rf"{'data'}.*", root).group()
-            data_paths.append(os.path.join(root_from_data, file))
-    data_df = pd.DataFrame(data_paths, columns=['Paths'])
+            root_from_data = root_from_data.replace('\\', '/')
+            data_paths.append(f'{root_from_data}/{file}')
+    data_df = pd.DataFrame(data_paths, columns=['Data_Paths'])
 
     # Read the bag manifest into a dataframe.
-    manifest_df = pd.read_csv(os.path.join(bag, 'manifest-md5.txt'), sep='  ', engine='python', names=['MD5', 'Paths'])
+    manifest_df = pd.read_csv(os.path.join(bag, 'manifest-md5.txt'), sep='  ', engine='python',
+                              names=['MD5', 'Manifest_Paths'])
 
     # Compare the data path list and the bag manifest, and return those only in the data path list.
-    data_only = data_df.merge(manifest_df, how='left')
-    extras = data_only['Paths'].tolist()
+    compare_df = data_df.merge(manifest_df, left_on='Data_Paths', right_on='Manifest_Paths', how='left')
+    data_only = compare_df[compare_df['Manifest_Paths'].isnull()]
+    extras = data_only['Data_Paths'].tolist()
     return extras
 
 
