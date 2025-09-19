bdd4f616b3a1aeb449558bbee86e8c5ef26825ac
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-19 15:46:32 -0400

    Make tests for find_extra_files

--- tests/test_delete_new_temp/test_file_extra_files.py
+++ tests/test_delete_new_temp/test_file_extra_files.py
@@ -5,11 +5,23 @@ from delete_new_temp import find_extra_files
 
 class MyTestCase(unittest.TestCase):
 
-    def test_function(self):
-        bag_path = os.path.join(os.getcwd(), 'test_data', 'find_extra_files', 'extra_bag')
+    def test_extra_not_temp(self):
+        bag_path = os.path.join(os.getcwd(), 'extra_not_temp_bag')
         extra_files = find_extra_files(bag_path)
+        expected = ['data/Extra.txt', 'data/Folder/Extra2.txt']
+        self.assertEqual(expected, extra_files, "Problem with test for extra_not_temp")
 
-        self.assertEqual(True, True)  # add assertion here
+    def test_extra_temp(self):
+        bag_path = os.path.join(os.getcwd(), 'extra_temp_bag')
+        extra_files = find_extra_files(bag_path)
+        expected = ['data/.Document.txt', 'data/Document.tmp', 'data/Folder/Thumbs.db']
+        self.assertEqual(expected, extra_files, "Problem with test for extra_temp")
+
+    def test_no_extra(self):
+        bag_path = os.path.join(os.getcwd(), 'no_extra_bag')
+        extra_files = find_extra_files(bag_path)
+        expected = []
+        self.assertEqual(expected, extra_files, "Problem with test for extra_not_temp")
 
 
 if __name__ == '__main__':
