bf57b55b3ac006d4c33e1fd6333478f0295f58d0
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-22 11:36:44 -0400

    Parse manifest with double spaces in paths

--- delete_new_temp.py
+++ delete_new_temp.py
@@ -38,7 +38,7 @@ def find_extra_files(bag):
     Returns: extras (list) - list of the paths for every file in data but not the manifest
     """
     # List of file paths in the data folder, saved as a dataframe to compare to manifest.
-    # To match the manifest, it needs to start at data and use forward slashes.
+    # To match the manifest, the path needs to start at data and use forward slashes.
     data_paths = []
     for root, dirs, files in os.walk(os.path.join(bag, 'data')):
         for file in files:
@@ -48,8 +48,11 @@ def find_extra_files(bag):
     data_df = pd.DataFrame(data_paths, columns=['Data_Paths'])
 
     # Read the bag manifest into a dataframe.
-    manifest_df = pd.read_csv(os.path.join(bag, 'manifest-md5.txt'), sep='  ', engine='python',
+    # data (start of the path) has to be used as part of the separator because paths may also include a double space,
+    # and needs to be added back so future paths are correct.
+    manifest_df = pd.read_csv(os.path.join(bag, 'manifest-md5.txt'), sep='  data', engine='python',
                               names=['MD5', 'Manifest_Paths'])
+    manifest_df['Manifest_Paths'] = 'data' + manifest_df['Manifest_Paths']
 
     # Compare the data path list and the bag manifest, and return those only in the data path list.
     compare_df = data_df.merge(manifest_df, left_on='Data_Paths', right_on='Manifest_Paths', how='left')
