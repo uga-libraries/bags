d0d7f507b8d9b09155b199d8dd0b14d6d08203ca
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-25 12:24:27 -0400

    Update test with new report name, column names, Source values

--- tests/test_bag_manifest_compare_files.py
+++ tests/test_bag_manifest_compare_files.py
@@ -18,11 +18,9 @@ class MyTestCase(unittest.TestCase):
 
     def tearDown(self):
         """Deletes the manifest compare report, if it was made"""
-        bags = ['add_bag', 'both_bag', 'delete_bag']
-        for bag in bags:
-            report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', f'{bag}_manifest_compare_report.csv')
-            if os.path.exists(report_path):
-                os.remove(report_path)
+        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'bag_manifest_compare_files_report.csv')
+        if os.path.exists(report_path):
+            os.remove(report_path)
 
     def test_add(self):
         """Test for when files were added since the bag was made"""
@@ -33,13 +31,13 @@ class MyTestCase(unittest.TestCase):
         subprocess.run(f'python {script_path} {bag_path}')
 
         # Tests the manifest compare report has the correct information.
-        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'add_bag_manifest_compare_report.csv')
+        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'bag_manifest_compare_files_report.csv')
         result = csv_to_list(report_path)
-        expected = [['paths', 'path_location'],
-                    ['data/0.txt', 'data'],
-                    ['data/6.txt', 'data'],
-                    ['data/7.txt', 'data']]
-        self.assertEqual(result, expected, "Problem with test for add")
+        expected = [['Path', 'Source'],
+                    ['data/0.txt', 'Data Folder'],
+                    ['data/6.txt', 'Data Folder'],
+                    ['data/7.txt', 'Data Folder']]
+        self.assertEqual(expected, result, "Problem with test for add")
 
     def test_both(self):
         """Test for when files were added and other files deleted since the bag was made"""
@@ -50,15 +48,15 @@ class MyTestCase(unittest.TestCase):
         subprocess.run(f'python {script_path} {bag_path}')
 
         # Tests the manifest compare report has the correct information.
-        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'both_bag_manifest_compare_report.csv')
+        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'bag_manifest_compare_files_report.csv')
         result = csv_to_list(report_path)
-        expected = [['paths', 'path_location'],
-                    ['data/0.txt', 'data'],
-                    ['data/1.txt', 'manifest'],
-                    ['data/2.txt', 'manifest'],
-                    ['data/5.txt', 'manifest'],
-                    ['data/6.txt', 'data']]
-        self.assertEqual(result, expected, "Problem with test for both")
+        expected = [['Path', 'Source'],
+                    ['data/0.txt', 'Data Folder'],
+                    ['data/1.txt', 'Manifest'],
+                    ['data/2.txt', 'Manifest'],
+                    ['data/5.txt', 'Manifest'],
+                    ['data/6.txt', 'Data Folder']]
+        self.assertEqual(expected, result, "Problem with test for both")
 
     def test_delete(self):
         """Test for when files were deleted since the bag was made"""
@@ -69,14 +67,14 @@ class MyTestCase(unittest.TestCase):
         subprocess.run(f'python {script_path} {bag_path}')
 
         # Tests the manifest compare report has the correct information.
-        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'delete_bag_manifest_compare_report.csv')
+        report_path = os.path.join(os.getcwd(), 'test_bag_manifest_compare_files', 'bag_manifest_compare_files_report.csv')
         result = csv_to_list(report_path)
-        expected = [['paths', 'path_location'],
-                    ['data/1.txt', 'manifest'],
-                    ['data/3.txt', 'manifest'],
-                    ['data/4.txt', 'manifest'],
-                    ['data/5.txt', 'manifest']]
-        self.assertEqual(result, expected, "Problem with test for delete")
+        expected = [['Path', 'Source'],
+                    ['data/1.txt', 'Manifest'],
+                    ['data/3.txt', 'Manifest'],
+                    ['data/4.txt', 'Manifest'],
+                    ['data/5.txt', 'Manifest']]
+        self.assertEqual(expected, result, "Problem with test for delete")
 
 
 if __name__ == '__main__':
