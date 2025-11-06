0d8aa437fd3a685e91dd637456b7859ba5469523
Adriane Hanson <ahansonuga@gmail.com>
2025-11-06 16:34:09 -0500

    Batch bags issue#19 (#20)
    * Create batch_bag.py
    
    * Test basic scenario
    
    * Adjust comments and line spacing
    
    * Make a log of the validation outcome
    
    * Add the log to the test
    
    * Fix command line call for bagit
    
    * Skip folders that will contain bags
    
    * Test for folder that will contain bags
    
    * Skip any file, not just log
    
    These should be put in a folder by the archivist before running the script
    
    * Test for skipping files
    
    * Rename function to be more descriptive
    
    * Update docstrings and comments
    
    * Add batch_bag.py to README

---
+++ batch_bag.py
@@ -0,0 +1,72 @@
+"""Make each folder in the provided directory into a bag with MD5 fixity and validate it, with logging.
+
+This script is primarily used in the accessioning workflow when an accession is too big for a single bag.
+It will skip any folder that ends with "_bags", which is used if a folder needs to be further split into bags,
+and any files that are not  in folders, which should be foldered by the archivist prior to running the script.
+
+Parameter:
+    bag_directory (required): path to the directory that contains the folders to be bagged
+
+Returns:
+    bag_validation_log.csv in the bag_directory
+"""
+import bagit
+import csv
+import os
+import sys
+
+
+def make_log(bag_path, note, new_log=False):
+    """Make or add to a log with validation results for each bag, saved to the bag_directory
+    Parameters:
+        bag_path (string) - path to bag_dir (if header) or specific bag
+        note (string) - output of bagit or standard text for the Notes column
+        new_log (Boolean, optional) - True if a new log should be started with a header
+    Returns: None
+    """
+    if new_log:
+        log_path = os.path.join(bag_path, 'bag_validation_log.csv')
+        with open(log_path, 'w', newline='') as log:
+            log_writer = csv.writer(log)
+            log_writer.writerow(['Bag', 'Valid?', 'Notes'])
+    else:
+        log_path = os.path.join(os.path.dirname(bag_path), 'bag_validation_log.csv')
+        with open(log_path, 'a', newline='') as log:
+            log_writer = csv.writer(log)
+            log_writer.writerow([os.path.basename(bag_path), note == 'Valid', note])
+
+
+def validate_bag(bag_path):
+    """Validate a bag and return the result
+    Parameter: bag_path (string) - path to bag
+    Returns: "Valid" or bagit error output
+    """
+    bag_instance = bagit.Bag(bag_path)
+    try:
+        bag_instance.validate()
+        return "Valid"
+    except bagit.BagValidationError as errors:
+        return errors
+
+
+if __name__ == '__main__':
+
+    bag_dir = sys.argv[1]
+    make_log(bag_dir, None, new_log=True)
+    for folder in os.listdir(bag_dir):
+        folder_path = os.path.join(bag_dir, folder)
+
+        # Skip all files and any folders that are big enough that the subfolders will be bags instead.
+        # They are still added to the log for checking that they should be been skipped.
+        if os.path.isfile(folder_path) or folder_path.endswith('_bags'):
+            make_log(folder_path, 'Skipped')
+            continue
+
+        # Make bag and rename it to add "_bag" according to standard naming conventions.
+        # Since these are for the backlog and not for preservation, we only use the MD5 checksum.
+        bagit.make_bag(folder_path, checksums=['md5'])
+        os.replace(folder_path, f'{folder_path}_bag')
+
+        # Validate the bag and log the result.
+        bagit_output = validate_bag(f'{folder_path}_bag')
+        make_log(f'{folder_path}_bag', bagit_output)
---
+++ tests/test_batch_bag.py
@@ -0,0 +1,102 @@
+import os
+import shutil
+import subprocess
+import unittest
+from test_bag_manifest_compare_files import csv_to_list
+
+
+def make_folder_list(path):
+    """Makes a list of all the folders in the bag_dir"""
+    dir_list = []
+    for root, dirs, files in os.walk(path):
+        for dir_name in dirs:
+            dir_list.append(dir_name)
+    return dir_list
+
+
+class MyTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        """Delete copy of test data (bagged or not), if made"""
+        bag_dir = os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir')
+        if os.path.exists(bag_dir):
+            shutil.rmtree(bag_dir)
+
+    def test_bag_all(self):
+        """Test for when all folders should be bagged"""
+        # Make a copy of the test data (script edits files).
+        shutil.copytree(os.path.join(os.getcwd(), 'test_batch_bag', 'bag_all'),
+                        os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir'))
+
+        # Make variables and run the script.
+        script_path = os.path.join('..', 'batch_bag.py')
+        bag_dir = os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir')
+        subprocess.run(f'python {script_path} {bag_dir}', shell=True)
+
+        # Test for the directory contents.
+        result = make_folder_list(bag_dir)
+        expected = ['aip1_bag', 'aip2_bag', 'data', 'data', 'aip2_subfolder']
+        self.assertEqual(expected, result, "Problem with test for bag_all, directory")
+
+        # Test for the log contents.
+        result = csv_to_list(os.path.join(bag_dir, 'bag_validation_log.csv'))
+        expected = [['Bag', 'Valid?', 'Notes'],
+                    ['aip1_bag', 'True', 'Valid'],
+                    ['aip2_bag', 'True', 'Valid'],
+                    ['bag_validation_log.csv', 'False', 'Skipped']]
+        self.assertEqual(expected, result, "Problem with test for bag_all, log")
+
+    def test_skip_bags(self):
+        """Test for when not all folders should be bagged"""
+        # Make a copy of the test data (script edits files).
+        shutil.copytree(os.path.join(os.getcwd(), 'test_batch_bag', 'skip_bags'),
+                        os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir'))
+
+        # Make variables and run the script.
+        script_path = os.path.join('..', 'batch_bag.py')
+        bag_dir = os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir')
+        subprocess.run(f'python {script_path} {bag_dir}', shell=True)
+
+        # Test for the directory contents.
+        result = make_folder_list(bag_dir)
+        expected = ['folder1_bag', 'folder2_bags', 'folder3_bag', 'data', 'folder2a', 'folder2b', 'data']
+        self.assertEqual(expected, result, "Problem with test for skip_bags, directory")
+
+        # Test for the log contents.
+        result = csv_to_list(os.path.join(bag_dir, 'bag_validation_log.csv'))
+        expected = [['Bag', 'Valid?', 'Notes'],
+                    ['bag_validation_log.csv', 'False', 'Skipped'],
+                    ['folder1_bag', 'True', 'Valid'],
+                    ['folder2_bags', 'False', 'Skipped'],
+                    ['folder3_bag', 'True', 'Valid']]
+        self.assertEqual(expected, result, "Problem with test for skip_bags, log")
+
+    def test_skip_files(self):
+        """Test for when the bag_dir includes loose files (not bagged)"""
+        # Make a copy of the test data (script edits files).
+        shutil.copytree(os.path.join(os.getcwd(), 'test_batch_bag', 'skip_files'),
+                        os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir'))
+
+        # Make variables and run the script.
+        script_path = os.path.join('..', 'batch_bag.py')
+        bag_dir = os.path.join(os.getcwd(), 'test_batch_bag', 'bag_dir')
+        subprocess.run(f'python {script_path} {bag_dir}', shell=True)
+
+        # Test for the directory contents.
+        # This is just testing for folders, so it won't list the loose files.
+        result = make_folder_list(bag_dir)
+        expected = ['aip1_bag', 'data']
+        self.assertEqual(expected, result, "Problem with test for skip_files, directory")
+
+        # Test for the log contents.
+        result = csv_to_list(os.path.join(bag_dir, 'bag_validation_log.csv'))
+        expected = [['Bag', 'Valid?', 'Notes'],
+                    ['a test file.txt', 'False', 'Skipped'],
+                    ['aip1_bag', 'True', 'Valid'],
+                    ['bag_validation_log.csv', 'False', 'Skipped'],
+                    ['Test_File.txt', 'False', 'Skipped']]
+        self.assertEqual(expected, result, "Problem with test for skip_files, log")
+
+
+if __name__ == '__main__':
+    unittest.main()
