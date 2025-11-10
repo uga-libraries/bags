7c1b123d99eca3b8a2e17d0ca1ac7f98d1b8f0b0
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-10 15:55:40 -0500

    Add make_directory_list to shared functions doc

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,6 @@
 """Functions used by more than one test for reading and formatting the test results"""
 
+import os
 import pandas as pd
 
 
@@ -9,3 +10,15 @@ def csv_to_list(csv_path):
     df = df.fillna('BLANK')
     csv_list = [df.columns.tolist()] + df.values.tolist()
     return csv_list
+
+
+def make_directory_list(path):
+    """Makes a list of the full paths for all the folders and files in the specified directory"""
+    dir_list = []
+    for root, dirs, files in os.walk(path):
+        for dir_name in dirs:
+            dir_list.append(os.path.join(root, dir_name))
+        for file_name in files:
+            dir_list.append(os.path.join(root, file_name))
+    dir_list.sort()
+    return dir_list
