39ff9b080419c10d908906924390dac502c8f240
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-10 12:42:50 -0500

    Test entire printed message now date not included

--- tests/test_validate_bags.py
+++ tests/test_validate_bags.py
@@ -16,13 +16,12 @@ class MyTestCase(unittest.TestCase):
         # Tests that the script output is correct.
         # Tests if key information is in the string rather than the entire string
         # because bagit.py output includes the exact time of the error, so it is different every time the test is run.
-        output = script_output.stdout.decode('utf-8')
-        result = [f'Bag invalid: {os.path.join(bag_directory, "aip_1_bag")}' in output,
-                  'Payload-Oxum validation failed. Expected 1 files and 19 bytes but found 1 files and 42 bytes' in output,
-                  f'Bag invalid: {os.path.join(bag_directory, "aip_2_bag")}' in output,
-                  'Payload-Oxum validation failed. Expected 2 files and 51 bytes but found 1 files and 26 bytes' in output]
-        expected = [True, True, True, True]
-        self.assertEqual(result, expected, "Problem with test for all invalid")
+        result = script_output.stdout.decode('utf-8')
+        expected = (f'\r\nBag invalid: {os.path.join(bag_directory, "aip_1_bag")} '
+                    f'Payload-Oxum validation failed. Expected 1 files and 19 bytes but found 1 files and 42 bytes\r\n'
+                    f'\r\nBag invalid: {os.path.join(bag_directory, "aip_2_bag")} '
+                    f'Payload-Oxum validation failed. Expected 2 files and 51 bytes but found 1 files and 26 bytes\r\n')
+        self.assertEqual(expected, result, "Problem with test for all invalid")
 
     def test_all_valid(self):
         """Test for when there are multiple bags in the directory, all valid."""
@@ -32,9 +31,10 @@ class MyTestCase(unittest.TestCase):
         script_output = subprocess.run(f'python {script_path} {bag_directory}', stdout=subprocess.PIPE, shell=True)
 
         # Tests that the script output is correct.
-        expected_output = f'\r\nBag valid: {os.path.join(bag_directory, "aip_1_bag")}\r\n\r\n' \
-                          f'Bag valid: {bag_directory}\\aip_2_bag\r\n'
-        self.assertEqual(script_output.stdout.decode('utf-8'), expected_output, "Problem with test for all valid")
+        result = script_output.stdout.decode('utf-8')
+        expected = f'\r\nBag valid: {os.path.join(bag_directory, "aip_1_bag")}\r\n\r\n' \
+                   f'Bag valid: {bag_directory}\\aip_2_bag\r\n'
+        self.assertEqual(expected, result, "Problem with test for all valid")
 
     def test_mix(self):
         """Test for when there are multiple things in the directory: valid bag, invalid bag, and not a bag."""
@@ -46,12 +46,11 @@ class MyTestCase(unittest.TestCase):
         # Tests that the script output is correct.
         # Tests if key information is in the string rather than the entire string
         # because bagit.py output includes the exact time of the error, so it is different every time the test is run.
-        output = script_output.stdout.decode('utf-8')
-        result = [f'Bag valid: {os.path.join(bag_directory, "aip_1_bag")}' in output,
-                  f'Bag invalid: {os.path.join(bag_directory, "aip_2_bag")}' in output,
-                  'Payload-Oxum validation failed. Expected 2 files and 51 bytes but found 1 files and 25 bytes' in output]
-        expected = [True, True, True]
-        self.assertEqual(result, expected, "Problem with test for mix")
+        result = script_output.stdout.decode('utf-8')
+        expected = (f'\r\nBag valid: {os.path.join(bag_directory, "aip_1_bag")}\r\n'
+                    f'\r\nBag invalid: {os.path.join(bag_directory, "aip_2_bag")} '
+                    'Payload-Oxum validation failed. Expected 2 files and 51 bytes but found 1 files and 25 bytes\r\n')
+        self.assertEqual(expected, result, "Problem with test for mix")
 
     def test_one(self):
         """Test for when there is only one bag in the directory, which is valid."""
@@ -61,8 +60,9 @@ class MyTestCase(unittest.TestCase):
         script_output = subprocess.run(f'python {script_path} {bag_directory}', stdout=subprocess.PIPE, shell=True)
 
         # Tests that the script output is correct.
-        expected_output = f'\r\nBag valid: {os.path.join(bag_directory, "aip_1_bag")}\r\n'
-        self.assertEqual(script_output.stdout.decode('utf-8'), expected_output, "Problem with test for all valid")
+        result = script_output.stdout.decode('utf-8')
+        expected = f'\r\nBag valid: {os.path.join(bag_directory, "aip_1_bag")}\r\n'
+        self.assertEqual(expected, result, "Problem with test for one valid")
 
 
 if __name__ == '__main__':
