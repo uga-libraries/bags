bd2072e2dd8383714de662e7ee3ac2a4d60e3ef4
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-10 14:27:08 -0500

    Reference log in top comment

--- validate_bags.py
+++ validate_bags.py
@@ -1,4 +1,4 @@
-"""Validate all bags at any level in a directory structure and print the result
+"""Validate all bags at any level in a directory structure and saves the result to a log
 
 Use this script to skip reading through the extra text printed by bagit.py validation.
 Bags must be named with "_bag" on the end to be detected as a bag by the script.
@@ -7,8 +7,7 @@ Parameters:
     bag_directory (required): path to the directory with the bags
 
 Returns:
-    If the bag is valid, it will print the bag name and that it is valid.
-    If the bag is invalid, it will print the bag name, that it is invalid, and the error message
+    Makes bag_validation_log.csv in bag_directory
 """
 import bagit
 import csv
