678918e64549b1d1957a4e9aa7580b7415b708cb
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-12 15:18:54 -0500

    Delete unnecessary tests

--- tests/test_delete_thumbs_db.py
+++ tests/test_delete_thumbs_db.py
@@ -64,60 +64,6 @@ class MyTestCase(unittest.TestCase):
                     [os.path.join(test_dir, 'aip3_bag'), 'True', 'BLANK']]
         self.assertEqual(expected, result, "Problem with test for batch, log")
 
-    def test_manifest(self):
-        """Test for when 1 Thumbs.db file is in the bag manifest"""
-        # Make a copy of the test data (script edits files).
-        shutil.copytree(os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'manifest_bag'), 
-                        os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'test_bag'))
-
-        # Make variables and run the script.
-        script_path = os.path.join('..', 'delete_thumbs_db.py')
-        bag_path = os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'test_bag')
-        printed = subprocess.run(f'python {script_path} {bag_path}', shell=True, capture_output=True, text=True)
-
-        # Test for the directory contents.
-        result = make_directory_list(bag_path)
-        expected = [os.path.join(bag_path, 'bag-info.txt'),
-                    os.path.join(bag_path, 'bagit.txt'),
-                    os.path.join(bag_path, 'data'),
-                    os.path.join(bag_path, 'data', 'Document.txt'),
-                    os.path.join(bag_path, 'manifest-md5.txt'),
-                    os.path.join(bag_path, 'tagmanifest-md5.txt')]
-        self.assertEqual(expected, result, "Problem with test for manifest, directory")
-
-        # Test for the printed information.
-        result = printed.stdout
-        expected = '\nBag is valid\n'
-        self.assertEqual(expected, result, "Problem with test for manifest, printed text")
-
-    def test_not_manifest(self):
-        """Test for when 2 Thumbs.db files are in the bag but not the manifest"""
-        # Make a copy of the test data (script edits files).
-        shutil.copytree(os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'not_manifest_bag'),
-                        os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'test_bag'))
-
-        # Make variables and run the script.
-        script_path = os.path.join('..', 'delete_thumbs_db.py')
-        bag_path = os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'test_bag')
-        printed = subprocess.run(f'python {script_path} {bag_path}', shell=True, capture_output=True, text=True)
-
-        # Test for the directory contents.
-        result = make_directory_list(bag_path)
-        expected = [os.path.join(bag_path, 'bag-info.txt'),
-                    os.path.join(bag_path, 'bagit.txt'),
-                    os.path.join(bag_path, 'data'),
-                    os.path.join(bag_path, 'data', 'Document.txt'),
-                    os.path.join(bag_path, 'data', 'Folder'),
-                    os.path.join(bag_path, 'data', 'Folder', 'Document.txt'),
-                    os.path.join(bag_path, 'manifest-md5.txt'),
-                    os.path.join(bag_path, 'tagmanifest-md5.txt')]
-        self.assertEqual(expected, result, "Problem with test for not_manifest, directory")
-
-        # Test for the printed information.
-        result = printed.stdout
-        expected = '\nBag is valid\n'
-        self.assertEqual(expected, result, "Problem with test for not_manifest, printed text")
-
 
 if __name__ == '__main__':
     unittest.main()
