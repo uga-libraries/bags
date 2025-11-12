24f88f40fd651a920accde3c09aa15b698d777e6
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-12 15:28:32 -0500

    Test for one AIP

--- tests/test_delete_thumbs_db.py
+++ tests/test_delete_thumbs_db.py
@@ -64,6 +64,38 @@ class MyTestCase(unittest.TestCase):
                     [os.path.join(test_dir, 'aip3_bag'), 'True', 'BLANK']]
         self.assertEqual(expected, result, "Problem with test for batch, log")
 
+    def test_one(self):
+        """Test for removing Thumbs.db from one bag"""
+        # Make a copy of the test data (script edits files).
+        shutil.copytree(os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'one'),
+                        os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'test_dir'))
+
+        # Make variables and run the script.
+        script_path = os.path.join('..', 'delete_thumbs_db.py')
+        bag_list = os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'one_list.txt')
+        subprocess.run(f'python {script_path} {bag_list}', shell=True)
+
+        # Test for the directory contents.
+        test_dir = os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'test_dir')
+        result = make_directory_list(test_dir)
+        expected = [os.path.join(test_dir, 'ER001_bag'),
+                    os.path.join(test_dir, 'ER001_bag', 'bag-info.txt'),
+                    os.path.join(test_dir, 'ER001_bag', 'bagit.txt'),
+                    os.path.join(test_dir, 'ER001_bag', 'data'),
+                    os.path.join(test_dir, 'ER001_bag', 'data', 'Document.txt'),
+                    os.path.join(test_dir, 'ER001_bag', 'data', 'Folder'),
+                    os.path.join(test_dir, 'ER001_bag', 'data', 'Folder2'),
+                    os.path.join(test_dir, 'ER001_bag', 'data', 'Folder', 'Document.txt'),
+                    os.path.join(test_dir, 'ER001_bag', 'manifest-md5.txt'),
+                    os.path.join(test_dir, 'ER001_bag', 'tagmanifest-md5.txt')]
+        self.assertEqual(expected, result, "Problem with test for one, directory")
+
+        # Test for the log contents.
+        result = csv_to_list(os.path.join(os.getcwd(), 'test_delete_thumbs_db', 'bag_validation_log.csv'))
+        expected = [['Bag_Path', 'Valid?', 'Notes'],
+                    [os.path.join(test_dir, 'ER001_bag'), 'True', 'BLANK']]
+        self.assertEqual(expected, result, "Problem with test for one, log")
+
 
 if __name__ == '__main__':
     unittest.main()
