bf3025ba8f43f9d93262152f89075c2084a74216
Adriane M Hanson <ahansonuga@gmail.com>
2026-05-19 15:14:18 -0400

    Add if __name__ == 'main'

--- check_file_count.py
+++ check_file_count.py
@@ -16,24 +16,27 @@ import csv
 import os
 import sys
 
-input_dir = sys.argv[1]
 
-log_path = os.path.join(input_dir, 'file_count_check.csv')
-with open(log_path, 'w', newline='') as log:
-    log_writer = csv.writer(log)
-    log_writer.writerow(['Folder', 'Files', 'Files_OK'])
+if __name__ == '__main__':
 
-for folder_name in os.listdir(input_dir):
-    # Skips metadata files.
-    if folder_name.endswith('.csv'):
-        continue
-    
-    # Gets the number of files at all levels.
-    file_count = 0 
-    for root, dirs, files in os.walk(os.path.join(input_dir, folder_name)):
-       file_count += len(files)
-   
-    # Saves size information to a log, including comparing it to the desired maximum.
-    with open(log_path, 'a', newline='') as log:
+    input_dir = sys.argv[1]
+
+    log_path = os.path.join(input_dir, 'file_count_check.csv')
+    with open(log_path, 'w', newline='') as log:
         log_writer = csv.writer(log)
-        log_writer.writerow([folder_name, file_count, file_count < 10000])
+        log_writer.writerow(['Folder', 'Files', 'Files_OK'])
+
+    for folder_name in os.listdir(input_dir):
+        # Skips metadata files.
+        if folder_name.endswith('.csv'):
+            continue
+
+        # Gets the number of files at all levels.
+        file_count = 0
+        for root, dirs, files in os.walk(os.path.join(input_dir, folder_name)):
+           file_count += len(files)
+
+        # Saves size information to a log, including comparing it to the desired maximum.
+        with open(log_path, 'a', newline='') as log:
+            log_writer = csv.writer(log)
+            log_writer.writerow([folder_name, file_count, file_count < 10000])
